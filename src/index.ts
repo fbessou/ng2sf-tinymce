925605f52bda50a15a0a96b3c25abcd106fb8e27
Sune Broendum Woeller <sune@woeller.dk>
2017-12-01 21:01:36 +0100

    export these also - for overriding the widget

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { Ng2SFTinyMCEModule } from "./ng2sf-tinymce.module";
 export { TinyMCEWidget } from "./tinymce.widget";
+export { TinyMCEComponent } from "./tinymce.component";
+export { TinyMCEValueAccessor } from "./tinymce.valueaccessor";
+export { TINYMCE_URL } from "./tinymce.component";
