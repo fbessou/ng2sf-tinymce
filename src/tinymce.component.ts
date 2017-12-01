f186d41452b9cf73e6bef07f9d726747e7b398cc
Sune Broendum Woeller <sune@woeller.dk>
2017-12-01 21:02:38 +0100

    angular 5 compatibility: use InjectionToken instead of OpaqueToken; Use templateUrl; Make initialValue public; Pass all options to tinymce, enabling configuration of plugins, language, menubar, branding, style_formats etc.

--- src/tinymce.component.ts
+++ src/tinymce.component.ts
@@ -1,124 +1,125 @@
 import {
-	AfterViewInit,
-	Component,
-	EventEmitter,
-	Inject,
-	Input,
-	NgZone,
-	OnChanges,
-	OnDestroy,
-	OpaqueToken,
-	Optional,
-	Output
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  NgZone,
+  OnChanges,
+  OnDestroy,
+  InjectionToken,
+  Optional,
+  Output
 } from "@angular/core";
 
 declare var tinymce;
 import "tinymce/tinymce";
 import "tinymce/themes/modern/theme";
 
-export const TINYMCE_URL = new OpaqueToken("TINYMCE_URL");
+export const TINYMCE_URL = new InjectionToken<string>("TINYMCE_URL");
 
 @Component({
-	selector: "tinymce",
-	template: require("./tinymce.component.html"),
+  selector: "tinymce",
+  templateUrl: "./tinymce.component.html",
 })
 export class TinyMCEComponent implements OnChanges, AfterViewInit, OnDestroy {
 
-	@Input() readonly: boolean = false;
-	@Input() id: string;
-	@Input() options: any = {plugins: "code"};
-	@Output() contentChange = new EventEmitter();
-	@Output() blur = new EventEmitter();
-	@Output() focus = new EventEmitter();
+  @Input() readonly: boolean = false;
+  @Input() id: string;
+  @Input() options: any = {plugins: "code"};
+  @Output() contentChange = new EventEmitter();
+  @Output() blur = new EventEmitter();
+  @Output() focus = new EventEmitter();
 
-	baseURL: string = "";
+  baseURL: string = "";
 
-	private editor: any = null;
-	private initialValue: string = "";
-	
-	constructor(private zone: NgZone, @Inject(TINYMCE_URL) @Optional() TINYMCE_URL: string ) {
-		this.baseURL = TINYMCE_URL ? TINYMCE_URL : "//cdnjs.cloudflare.com/ajax/libs/tinymce/4.4.2";
-	}
+  private editor: any = null;
+  public initialValue: string = "";
 
-	ngAfterViewInit() {
-		this.createEditor();
-	}
+  constructor(private zone: NgZone, @Inject(TINYMCE_URL) @Optional() TINYMCE_URL: string ) {
+    this.baseURL = TINYMCE_URL ? TINYMCE_URL : "//cdnjs.cloudflare.com/ajax/libs/tinymce/4.4.2";
+  }
 
-	ngOnChanges(changes) {
-		if (this.editor && changes["readonly"]) {
-			this.removeEditor();
-			this.createEditor();
-		}
-	}
+  ngAfterViewInit() {
+    this.createEditor();
+  }
 
-	// TinyMCE triggers an error if the DOM elements it created are
-	// inexistent which is the case if this component if located in
-	// a structural block (*ngIf/*ngFor).
-	// We indicate the editor has already been removed from DOM
-	// so that destroy doesn"t call "remove" and we remove the editor
-	// from tinymce.
-	ngOnDestroy() {
-		this.editor.removed = true;
-		this.removeEditor();
-	}
+  ngOnChanges(changes) {
+    if (this.editor && changes["readonly"]) {
+      this.removeEditor();
+      this.createEditor();
+    }
+  }
 
-	private createEditor() {
-		tinymce.baseURL = this.baseURL;
-		let options: any = {
-			selector: "#" + this.id,
-			plugins: this.options.plugins,
-			readonly: this.readonly ? 1 : 0,
-			setup: (editor) => { this.editor = editor; this.bindEditor();}
-		};
+  // TinyMCE triggers an error if the DOM elements it created are
+  // inexistent which is the case if this component if located in
+  // a structural block (*ngIf/*ngFor).
+  // We indicate the editor has already been removed from DOM
+  // so that destroy doesn"t call "remove" and we remove the editor
+  // from tinymce.
+  ngOnDestroy() {
+    this.editor.removed = true;
+    this.removeEditor();
+  }
 
-		if (this.readonly) {
-			options.toolbar = options.menubar = false;
-		}
-		tinymce.init(options);
-	}
+  private createEditor() {
+    tinymce.baseURL = this.baseURL;
+    let options: any = {
+      selector: "#" + this.id,
+      readonly: this.readonly ? 1 : 0,
+      setup: (editor) => { this.editor = editor; this.bindEditor();}
+    };
 
-	private bindEditor() {
-		this.editor.on("change keyup cut paste undo redo", () => {
-			this.emitContentChange();
-		});
+    Object.assign(options, this.options)
 
-		this.editor.on("blur", () => {
-			this.zone.run(() => { this.blur.emit(true); return true; });
-		});
+    if (this.readonly) {
+      options.toolbar = options.menubar = false;
+    }
+    tinymce.init(options);
+  }
 
-		this.editor.on("focus", () => {
-			this.zone.run(() => { this.focus.emit(true); });
-		});
-	}
+  private bindEditor() {
+    this.editor.on("change keyup cut paste undo redo", () => {
+      this.emitContentChange();
+    });
 
-	removeEditor() {
-		// Hacks to avoid exceptions if the editor is destroyed
-		// before it is thoroughly initialized 
-		if (!this.editor.selection) {
-			this.editor.selection = {destroy: () => {} };
-			this.editor.selection.dom = { };
-		}
+    this.editor.on("blur", () => {
+      this.zone.run(() => { this.blur.emit(true); return true; });
+    });
 
-		if (!this.editor.dom) {
-			this.editor.dom = {destroy: () => {}};
-		}
+    this.editor.on("focus", () => {
+      this.zone.run(() => { this.focus.emit(true); });
+    });
+  }
 
-		this.editor.destroy();
-		tinymce.remove(this.editor);
-		this.editor = null;
-	}
+  removeEditor() {
+    // Hacks to avoid exceptions if the editor is destroyed
+    // before it is thoroughly initialized
+    if (!this.editor.selection) {
+      this.editor.selection = {destroy: () => {} };
+      this.editor.selection.dom = { };
+    }
 
-	private emitContentChange() {
-		let event = {value: this.editor.getContent()};
-		this.zone.run(() => {
-			this.contentChange.emit(event);
-		});
-	}
+    if (!this.editor.dom) {
+      this.editor.dom = {destroy: () => {}};
+    }
 
-	setContent(content: string) {
-		this.initialValue = content;
-		if (this.editor) {
-			this.editor.setContent(content);
-		}
-	}
+    this.editor.destroy();
+    tinymce.remove(this.editor);
+    this.editor = null;
+  }
+
+  private emitContentChange() {
+    let event = {value: this.editor.getContent()};
+    this.zone.run(() => {
+      this.contentChange.emit(event);
+    });
+  }
+
+  setContent(content: string) {
+    this.initialValue = content;
+    if (this.editor) {
+      this.editor.setContent(content);
+    }
+  }
 }
